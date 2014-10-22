e619fbe00bedba0a390cd029936778215461f817
Nícolas Iensen <nicolas@iensen.me>
2014-10-22 14:29:57 -0200

    [#80288298] change ActiveResource classes to work dynamically

--- lib/acts_as_our_cities/api/user.rb
+++ lib/acts_as_our_cities/api/user.rb
@@ -1,8 +1,16 @@
 module ActsAsOurCities
   module API
     class User < ActiveResource::Base
-      @headers  = { 'Authorization' => "Token token=\"#{ActsAsOurCities.api_token}\"" }
-      self.site = ActsAsOurCities.api_site
+      self.include_root_in_json = true
+
+      def self.headers
+        { 'Authorization' => "Token token=\"#{ActsAsOurCities.api_token}\"" }
+      end
+
+      def self.site
+        self.site = ActsAsOurCities.api_site
+        super
+      end
     end
   end
 end
